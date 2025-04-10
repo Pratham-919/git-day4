18d9f50c47b8edb38534e718102f23acdab9b0ed
Pratham-919 <prathmeshvyawhare19@gmail.com>
2025-04-10 21:24:44 +0530

    app.py updated by aws

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import boto3
 import zipfile
-import changes by pratham 
+import changes by aws  
 
 LAMBDA_FUNCTION_NAME = "helloLambdaBoto3"
 ROLE_ARN = "arn:aws:iam::123456789012:role/lambda-execution-role"  # replace with your actual role ARN
